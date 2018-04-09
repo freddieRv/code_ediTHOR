44a8b5a4558cb6ae7c6d19007f44b1897406e4ed
freddieRv <federico.ram.v@gmail.com>
2018-04-08 22:46:03 -0500

    added icons to buttons and added hide/show file-tree functionallity

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,5 +1,21 @@
 $(document).ready( function() {
     $("#hide_file_tree").click(function(){
         $("#file_tree").hide();
+        $("#editor").removeClass('col-md-10');
+        $("#editor").addClass('col-md-12');
+        $("#editor").removeClass('col-xs-11');
+        $("#editor").addClass('col-xs-12');
+        $(this).hide();
+        $("#show_file_tree").show();
+    });
+
+    $("#show_file_tree").click(function(){
+        $("#file_tree").show();
+        $("#editor").removeClass('col-md-12');
+        $("#editor").addClass('col-md-10');
+        $("#editor").removeClass('col-xs-12');
+        $("#editor").addClass('col-xs-11');
+        $(this).hide();
+        $("#hide_file_tree").show();
     });
 });
