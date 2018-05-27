b6620a2875397cd563853b51b0cdb56e192802c4
freddieRv <federico.ram.v@gmail.com>
2018-05-27 18:59:38 -0500

    connection to api

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,5 +1,129 @@
-var editor     = ace.edit("editor");
-var file_tree  = $("#file_tree");
+const LANG_HIGHLIGHT = {
+    'cpp':  'ace/mode/c_cpp',
+    'c':    'ace/mode/c_cpp',
+    'java': 'ace/mode/java',
+    'py':   'ace/mode/python',
+}
+
+var editor       = ace.edit("editor");
+var file_tree    = $("#file_tree");
+var current_file = 0;
+
+$("#btn_save").click(function() {
+    if (!current_file) {
+        swal({
+            type:  "warning",
+            title: "Nothing to save"
+        });
+        return;
+    }
+
+    $.ajax({
+        url: 'http://localhost:3000/files/' + current_file,
+        type: 'PUT',
+        dataType: 'json',
+        headers: {
+            'x-access-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTI3NDMxODYzLCJleHAiOjE1Mjc1MTgyNjN9.kXiWL-4qMCpkP3qjIhswOGP4vJUqclfbyogiwizXmVc"
+        },
+        data: {
+            content: btoa(editor.getValue()),
+        },
+        success: function(res) {
+            swal({
+                type:  "success",
+                title: "File successfuly saved"
+            });
+        },
+        fail: function(err) {
+            console.log(err);
+        }
+    });
+
+});
+
+$("#btn_update").click(function() {
+    if (!current_file) {
+        swal({
+            type:  "warning",
+            title: "Nothing to update"
+        });
+        return;
+    }
+
+    $.ajax({
+        url: 'http://localhost:3000/files/' + current_file,
+        type: 'GET',
+        headers: {
+            'x-access-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTI3NDMxODYzLCJleHAiOjE1Mjc1MTgyNjN9.kXiWL-4qMCpkP3qjIhswOGP4vJUqclfbyogiwizXmVc"
+        },
+        success: function(res) {
+            var decoded_content = atob(res.content);
+            var file_name       = res.name;
+            var file_ext        = file_name.split(".")[1];
+
+            current_file = res.id;
+
+            editor.setValue(decoded_content);
+            editor.session.setMode(LANG_HIGHLIGHT[file_ext]);
+        },
+        fail: function(err) {
+            console.log(err);
+        }
+    });
+
+});
+
+$("#btn_run").click(function() {
+    if (!current_file) {
+        swal({
+            type:  "warning",
+            title: "Nothing to execute"
+        });
+        return;
+    }
+
+    // Save file
+    $.ajax({
+        url: 'http://localhost:3000/files/' + current_file,
+        type: 'PUT',
+        dataType: 'json',
+        headers: {
+            'x-access-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTI3NDMxODYzLCJleHAiOjE1Mjc1MTgyNjN9.kXiWL-4qMCpkP3qjIhswOGP4vJUqclfbyogiwizXmVc"
+        },
+        data: {
+            content: btoa(editor.getValue()),
+        },
+        fail: function(err) {
+            swal({
+                type: 'error',
+                title: 'There was an error saving the file, please try again'
+            });
+        }
+    });
+
+    $.ajax({
+        url: 'http://localhost:3000/files/' + current_file + '/exec',
+        type: 'GET',
+        headers: {
+            'x-access-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTI3NDMxODYzLCJleHAiOjE1Mjc1MTgyNjN9.kXiWL-4qMCpkP3qjIhswOGP4vJUqclfbyogiwizXmVc"
+        },
+        dataType: 'json',
+        success: function(res) {
+            console.log(res);
+            var html = "";
+
+            for (var i = 0; i < res.console_log.length; i++) {
+                html += res.console_log[i] + "\n";
+            }
+
+            $("#console").html(html);
+        },
+        fail: function(err) {
+            console.log(err);
+        }
+    });
+
+});
 
 $("#hide_file_tree").click(function() {
     $("#file_tree").hide();
@@ -31,9 +155,7 @@ $.url_param = function(param) {
 
 $(document).ready( function() {
     editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/c_cpp");
     editor.session.setUseSoftTabs(true);
-
     editor.focus();
 
     var project_id = $.url_param('project_id');
@@ -42,15 +164,60 @@ $(document).ready( function() {
 
     file_tree.jstree({
         'core' : {
+            'check_callback' : true ,
             'data' : $.ajaxSetup({
                 type : 'GET',
                 headers : {
-                    // TODO: where to get this access token from?
-                    'x-access-token' : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNTI2ODY1OTQxLCJleHAiOjE1MjY5NTIzNDF9.7M4OVaJHLfk2EVG19lZprDkqCZ7JUFOOI_62uGRNeKU"
+                    'x-access-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTI3NDMxODYzLCJleHAiOjE1Mjc1MTgyNjN9.kXiWL-4qMCpkP3qjIhswOGP4vJUqclfbyogiwizXmVc"
                 },
                 'dataType' : "json",
                 'url' : api_url,
             }),
+            'contextmenu': {
+                'items': [
+                    {
+                        'label': 'Test',
+                        'action': function(e) {console.log(e);},
+                    }
+                ]
+            },
+            'types': {
+                'd': {
+                    'icon': "fa folder",
+                },
+                'f': {
+                    'icon': "fa file-code-o",
+                }
+            },
+            'plugins' : [
+                "contextmenu",
+                "types",
+            ],
+        }
+    });
+
+    file_tree.on("select_node.jstree", function (e, data) {
+        if (data.node.original.type == 'f') {
+            $.ajax({
+                url: 'http://localhost:3000/files/' + data.node.original.id,
+                type: 'GET',
+                headers: {
+                    'x-access-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTI3NDMxODYzLCJleHAiOjE1Mjc1MTgyNjN9.kXiWL-4qMCpkP3qjIhswOGP4vJUqclfbyogiwizXmVc"
+                },
+                success: function(res) {
+                    var decoded_content = atob(res.content);
+                    var file_name       = res.name;
+                    var file_ext        = file_name.split(".")[1];
+
+                    current_file = res.id;
+
+                    editor.setValue(decoded_content);
+                    editor.session.setMode(LANG_HIGHLIGHT[file_ext]);
+                },
+                fail: function(err) {
+                    console.log(err);
+                }
+            });
         }
     });
 
